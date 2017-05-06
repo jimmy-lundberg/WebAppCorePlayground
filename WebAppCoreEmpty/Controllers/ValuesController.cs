433775ff6c9dc15d73af1361a5b748699100337c
Jimmy Lundberg <jimmy-lundberg@hotmail.com>
2017-05-06 19:22:31 +0200

    Finished building RESTful API template.

--- WebAppCoreEmpty/Controllers/ValuesController.cs
+++ WebAppCoreEmpty/Controllers/ValuesController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebAppCoreRESTful.Controllers
+namespace WebAppCoreEmpty.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
