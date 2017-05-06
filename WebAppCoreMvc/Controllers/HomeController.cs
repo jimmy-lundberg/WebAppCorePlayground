45be7b18273fc07b56e140a601c27f1cf249da1f
Jimmy Lundberg <jimmy-lundberg@hotmail.com>
2017-05-06 16:27:04 +0200

    Created new empty project to turn it into API project.

--- WebAppCoreMvc/Controllers/HomeController.cs
+++ WebAppCoreMvc/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ namespace WebAppCoreMvc.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
