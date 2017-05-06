4a02a73bed621f806ca61636d4bc67caa63286c0
Jimmy Lundberg <jimmy-lundberg@hotmail.com>
2017-05-06 17:42:07 +0200

    Added dependencies and configuration files.

--- WebAppCoreEmpty/Program.cs
+++ WebAppCoreEmpty/Program.cs
@@ -9,17 +9,19 @@ namespace WebAppCoreEmpty
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .UseApplicationInsights()
-                .Build();
+        
+        
+        //public static void Main(string[] args)
+        //{
+        //    var host = new WebHostBuilder()
+        //        .UseKestrel()
+        //        .UseContentRoot(Directory.GetCurrentDirectory())
+        //        .UseIISIntegration()
+        //        .UseStartup<Startup>()
+        //        .UseApplicationInsights()
+        //        .Build();
 
-            host.Run();
-        }
+        //    host.Run();
+        //}
     }
 }
