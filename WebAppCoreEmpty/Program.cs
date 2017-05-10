904605431746148901769e5a06878aa3c1447a90
Jimmy Lundberg <jimmy-lundberg@hotmail.com>
2017-05-10 23:19:16 +0200

    Added webroot stuff, Program & Startup.

--- WebAppCoreEmpty/Program.cs
+++ WebAppCoreEmpty/Program.cs
@@ -9,5 +9,17 @@ namespace WebAppCoreEmpty
 {
     public class Program
     {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
     }
 }
--- WebAppCoreEmpty/Startup.cs
+++ WebAppCoreEmpty/Startup.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -12,11 +14,40 @@ namespace WebAppCoreEmpty
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsDevelopment())
+            {
+                // For more details on using the user secret store see https://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets<Startup>();
+            }
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        
+        public void ConfigureServies(IServiceCollection services)
+        {
+            // Add framework services
+            // services.AddDbContext<ApplicationDbContext>();
+
+            // Add application services
+
+        }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+
+        }
     }
 }
